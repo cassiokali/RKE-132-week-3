f89fac3980694fe9f4002df6a06138ba6ecdd260
cassiokali <nikita.lebedev@tktk.ee>
2026-02-22 14:36:53 +0200

    add week 3 files

---
+++ color_test.py
@@ -0,0 +1,13 @@
+color = input('Sisesta oma lemmikvärv:').lower()
+
+match color:
+    case 'punane':
+        print('')
+    case 'roosa':
+        print('')
+    case 'roheline':
+        print('')
+    case 'sinine':
+        print('')
+    case _:
+        print('')
