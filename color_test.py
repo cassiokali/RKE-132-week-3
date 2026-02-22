b495fd9ea44f347813a6ef574833c6eb5c1da589
cassiokali <nikita.lebedev@tktk.ee>
2026-02-22 14:37:40 +0200

    add week 3 files

--- color_test.py
+++ color_test.py
@@ -1,13 +1,25 @@
 color = input('Sisesta oma lemmikvärv:').lower()
 
+
+# if color == 'punane':
+#     print('Sa oled energeline inimene!')
+# elif color == 'roosa':
+#     print('Sa oled romantik!')
+# elif color == 'roheline':
+#      print('Sa oled rahulik inimene!')
+# elif color == 'sinine':
+#     print('Sa oled keskendunud inimene!')
+# else:
+#     print('Sa oled imeline ükssarvik!')
+
 match color:
     case 'punane':
-        print('')
+        print('Sa oled energeline inimene!')
     case 'roosa':
-        print('')
+        print('Sa oled romantik!')
     case 'roheline':
-        print('')
+        print('Sa oled rahulik inimene!')
     case 'sinine':
-        print('')
+        print('Sa oled keskendunud inimene!')
     case _:
-        print('')
+        print('Sa oled imeline ükssarvik!')
